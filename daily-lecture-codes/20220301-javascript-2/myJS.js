bdcac07489544b330a0f1a76b3a4a29d28f35a94
vinay.maurya1 <vinay.shermauryan@gmail.com>
2022-03-01 09:26:51 +0530

    javascript-part-2

---
+++ daily-lecture-codes/20220301-javascript-2/myJS.js
@@ -0,0 +1,35 @@
+console.log("Hello world");
+
+// 1.   = (asign) used to assign value
+//      == equal to  ( only values)
+//      === equa to (values and datatype)
+
+// 2. Syntax Termnation  -> user ( ; ) at end of syntax
+// 3. Identtation -> to keep the code in alignment
+
+// 4. identifiers
+// identifiers are used to  represent any variable, object, method, function
+
+// 5. variables
+// Variables hold the value. The value can be string, number, object, array, null, udefiend
+// variable can not be same as reserved keywords
+// variables can not be started from number, special character
+//  15cars  XXXX
+// %geOfMoney  XXXXX
+//  _time OKOK
+// listOfCars, calculation, Add2Numbers
+
+// 6. data-type
+//     Number 1, 78, -56, 0.2655 (without quotes)
+//     String 'car', 'javScript', 'I love JavaScript', '15', '@gmail.com'
+
+var myName = "vinay is my name'hii'";
+var newName = myName;
+
+var a = 10;
+var b = 20;
+var c = b;
+var d = a;
+console.log(a + b + c + d + myName);
+console.log(a + b + c + d, myName); // okok
+console.log("Here is the calculation:", a + b + c + d, "Welcome-->", myName); // okok
