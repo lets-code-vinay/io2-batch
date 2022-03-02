7e452abc5c36d98ffeb18dc4929b99fef5836fa2
vinay.maurya1 <vinay.shermauryan@gmail.com>
2022-03-02 09:28:05 +0530

    javascript-3-datatype,syymbols

--- daily-lecture-codes/20220301-javascript-2/myJS.js
+++ daily-lecture-codes/20220301-javascript-2/myJS.js
@@ -1,8 +1,8 @@
-console.log("Hello world");
+// console.log("Hello world");
 
 // 1.   = (asign) used to assign value
 //      == equal to  ( only values)
-//      === equa to (values and datatype)
+//      === equal to (values and datatype)
 
 // 2. Syntax Termnation  -> user ( ; ) at end of syntax
 // 3. Identtation -> to keep the code in alignment
@@ -20,9 +20,63 @@ console.log("Hello world");
 // listOfCars, calculation, Add2Numbers
 
 // 6. data-type
-//     Number 1, 78, -56, 0.2655 (without quotes)
-//     String 'car', 'javScript', 'I love JavaScript', '15', '@gmail.com'
+// Two types of data-type
+//  1. Primitive-datatype   2. Non-primitive datatype
 
+//----primitive datatype-----
+//     1. Number 1, 78, -56, 0.2655 (without quotes)
+//              All numeric values are number datatype.
+//     2. String 'car', 'javScript', 'I love JavaScript', '15', '@gmail.com'
+//              Any value store inside quotes, double quotes and literal template(back-tick) are string
+//     3. Boolean  ==> true/false
+//     4. udefined   ==>  when any variable is declared but there is no value
+//     5. null ==> null doesn't have any value itself
+
+//----Non-primitive datatype-----
+// 1. Object : keys: value
+// const animal = { dog: "Pug", cat: "Tom" };
+// 2.  Array : [] array is just like a container
+// const computer = ["LCD", "Kyboard", "Mouse", "CPU", 10, null, true, animal];
+// 3. regex ==> are coded  form /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+// ----------symbols---------
+// ( ` ) backtick
+// ( ~ ) tilde
+// ( ! ) sign of exclamation
+// ( @ ) At the rate
+// ( # ) hash
+// ( $ ) dollar sign
+// ( % ) percentage sign
+// ( ^ )
+// ( & ) ampersand
+// ( && ) AND
+// ( * ) astrisk
+// ( ( ) paranthisis open
+// ( ) ) paranthisis close
+// ( - ) Hyphen/Subtract/Minus
+// ( _ ) Undescore
+// ( = ) Asign
+// ( == ) Equal to (only value) (10 == '10' => true)
+// ( === ) Equal to ( value and data-type) (10 === '10' => false)
+// ( { ) Curly Braces Open
+// ( } ) Curly Braces Close
+// ( [ ) Squre Braces Open
+// ( ] ) Squre Braces Close
+// ( | ) Pipe/bar
+// ( || ) doube pipe == OR
+// ( \ ) back-slash
+// ( / ) forward-slash
+// ( : ) colon
+// ( ; ) semi-colon
+// ( ' ' ) Single-quotes
+// ( " " ) Double-quotes
+// ( < ) Angular Braces open/ less than
+// ( > ) Angular Braces close/ greater than
+// ( ? ) Question mark
+// ( . ) dot/fullstop
+// ( , ) comma
+// ( NaN ) Not A Number
+//
 var myName = "vinay is my name'hii'";
 var newName = myName;
 
@@ -30,6 +84,34 @@ var a = 10;
 var b = 20;
 var c = b;
 var d = a;
-console.log(a + b + c + d + myName);
-console.log(a + b + c + d, myName); // okok
-console.log("Here is the calculation:", a + b + c + d, "Welcome-->", myName); // okok
+var longName = "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.";
+// console.log(a + b + c + d + myName);
+// console.log(a + b + c + d, myName); // okok
+// console.log("Here is the calculation:", a + b + c + d, "Welcome-->", myName); // okok
+// --  -------------------------------------------------------------
+// console.log("M first A :", a);
+
+var a = 50;
+var a = 24;
+// console.log("Myfirst work: ", a);
+
+a = 15;
+// console.log("My second A: ", a);
+
+// ---------------------------------------
+
+// ES6 feture let/const
+let car = 120;
+console.log("car 1-->", car);
+
+car = "Swift";
+
+console.log("car-->", car);
+// -------------------------------------------------------------
+// const == constant
+
+let age = "abc";
+const newAge = age * 10;
+console.log("my age is:", newAge);
+
+// ------------------------------------------------------
