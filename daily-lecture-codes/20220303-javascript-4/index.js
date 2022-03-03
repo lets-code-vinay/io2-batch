4740af45d21e4c91ab660396453a814c02b65786
vinay.maurya1 <vinay.shermauryan@gmail.com>
2022-03-03 09:18:53 +0530

    javascript-day_4-arithmeetic operors

---
+++ daily-lecture-codes/20220303-javascript-4/index.js
@@ -0,0 +1,77 @@
+/**
+ * 
+ 1. Variables --Done
+        a. var
+        b. let
+        c. const
+ */
+/**
+ 2. Symbols  --Done 
+    "\t" -> used as tab
+    "\n"  => used as Enter to new line
+ */
+
+/**
+  3. data-type
+        a. String,
+        b. Number
+        c. Boolean
+  */
+
+/**
+  4. Operators
+        a. Arithmetic operator :- use for mathematical calculation
+            i. Addition ( + )
+            ii. Substraction ( - )
+            iii. Multipication ( * )
+            iv. Division ( / )
+            v. Modulus ( % ) => used to find remainder  
+            vi. Increment ( ++ ) ( +1 )
+            v. Decrement ( -- ) ( -1 )
+
+        b. Logical Opertors
+        c. comparison/Rational Operator
+        d. Assignment Operator
+        e. Bitwise Operator  XXXXX
+  */
+// ----Exercise ----
+// Find the intereset
+// principal = 1560 rs.
+// time = 12months,
+// intereset rate = 6%
+// Find he interest value
+
+// ----------Formula-------------------
+// p * r *t/100
+
+// ----------Solution-------------------
+// ---uncomment if you want to check solution
+// const principal = 1560;
+// const time = 12;
+// const rate = 6;
+
+// let interest;
+// interest = principal * time * rate;
+// interest = interest / 100;
+
+// console.log("The interest is:", interest);
+
+// const amountToPay = principal + interest;
+// console.log("My total amount to pay after one year:", amountToPay);
+
+// console.log('ravi"s amount', (1400 / 18000) * 100);
+// // ---------------------------------------------------------
+
+// const number1 = 1532;
+// const divider = 15;
+// console.log(
+//   "The divided value is:",
+//   1532 / 15,
+
+//   "\t",
+//   "the remainer is:",
+//   number1 % 15
+// );
+
+// ----------------Boolean----------------
+// True/false
