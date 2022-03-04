a5d58974d5d4b1c9dfcaca8b612fc86f6d4fa8d6
vinay.maurya1 <vinay.shermauryan@gmail.com>
2022-03-04 09:26:37 +0530

    20220304-lecture

---
+++ daily-lecture-codes/20220304-javascript-5/index.js
@@ -0,0 +1,43 @@
+/*
+temprature of a city in farhenheit degrees is input through the keyboard. write a program to convert the temprature into centigrde and centrigrade to farhenheit
+*/
+
+// Celsius to Fahrenheit: (9/5 × °C) + 32
+// Fahrenheit to Celsius: 5/9(°F - 32)
+// Kelvin to Celsius: K - 273
+// to kelvin : C + 273
+
+// const temp = prompt("Enter the Temprature:");
+
+// const far = (9 / 5) * temp + 32;
+// const cel = (5 / 9) * (temp - 32);
+// const kel = cel + 273;
+// console.log("Entered temprature is:", temp);
+// console.log("1. Temprature in Celcius:", cel.toFixed(2) + "deg");
+// // console.log(2. Temprature in Farhenheit: ", far, "deg");
+// console.log(
+//   "2. Temprature in Farhenheit: ",
+//   far.toFixed(2) + "deg"
+//   //   typeof far,
+//   //   typeof "deg"
+// );
+// console.log("3. temprature in Kelvin:", kel.toFixed(2) + "deg");
+
+// Concatnation
+// console.log("1.", Number("100") + String(500) + Number(200));
+// console.log("2.", 100 + "500");
+// console.log("3.", 1000 - 500);
+// console.log("4.", 1000 - "500");
+// console.log("5.", 1000 * "500");
+
+// Math--library
+// To get random Number Math.random()
+console.log(
+  "random numer is:",
+  Math.random() * 1000,
+  "and PI is :",
+  Math.PI,
+  Math.random() * 1000 * Math.PI
+);
+const abc = prompt("hello Enter anything");
+console.log(abc);
