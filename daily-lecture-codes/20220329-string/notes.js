f3c82e373ca271ce6d11414142ba0f3840dde312
Vinay <vinay.shermauryan@gmail.com>
2022-03-29 09:00:31 +0530

    2022-03-29 string-4

---
+++ daily-lecture-codes/20220329-string/notes.js
@@ -0,0 +1,22 @@
+/**
+ * String
+ */
+// What is string?
+// String is primitive datatype, Any characters wrapped inside the double-quotes or single-quotes are string
+// example: 'Kanak', 'Ravi123', '1234', "Vinay is here", "#$#%#"
+
+// ---> Primitive Datatypes <---
+// 1.  string
+// 2.  number
+// 3.  boolean(true/false)
+// 4.  null
+// 5.  undefined
+
+// ---> Non-Primitive Datatypes <---
+// 1.  Array
+// 2.  Object
+// 3.  regex`
+
+// 1.  NaN
+
+//
